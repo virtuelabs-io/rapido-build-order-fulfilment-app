5bddbcc03823a7ae20c794f381dede775532b546
Sangram Reddy <reddy.horcrux@gmail.com>
2020-05-24 14:34:09 +0100

    Configured babel path resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,27 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        [
+          "module-resolver",
+          {
+            "root": ["./src"],
+            alias: {
+                "_components/*": "./components",
+                "_atoms/*": "./components/atoms",
+                "_molecules/*": "./components/molecules",
+                "_organisms/*": "./components/organisms",
+                "_scenes/*": "./scenes",
+                "_navigations/*": "./navigations",
+                "_assets/*": "./assets",
+                "_images/*": "./assets/images",
+                "_fonts/*": "./assets/fonts",
+                "_services/*": "./services",
+                "_utils/*": "./utils",
+                "_styles/*": "./styles"
+            }
+          }
+        ]
+      ]
   };
 };
