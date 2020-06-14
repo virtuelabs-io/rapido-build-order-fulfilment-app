373367bc426e1ca0575398a7501b17d535cd1988
Sangram Reddy <reddy.horcrux@gmail.com>
2020-06-14 18:20:10 +0100

    testing scan option

--- src/scenes/scan-barcode-screen/styles.ts
+++ src/scenes/scan-barcode-screen/styles.ts
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 
 export const Styles = StyleSheet.create({
   screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+    }
 })
 
 export default Styles
--- src/scenes/scan-barcode-screen/view.tsx
+++ src/scenes/scan-barcode-screen/view.tsx
@@ -1,25 +1,66 @@
 import React from 'react'
-import { View, Button } from 'react-native'
-import { RText } from '../../components/atoms'
+import { View, Button, Text, StyleSheet } from 'react-native'
+import { RText, RButton } from '../../components/atoms'
 import Styles from './styles'
 import { ScanBarcodeScreenProps, ScanBarcodeScreenState } from './types'
 import { getStackStyles } from '../../commons/styles';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from 'expo-permissions';
 
 class ScanBarcodeScreen extends React.Component<ScanBarcodeScreenProps, ScanBarcodeScreenState> {
+
+    state = {
+        hasCameraPermission: null,
+        scanned: false,
+    }
+
     constructor(props: ScanBarcodeScreenProps) {
         super(props)
         this.props.navigation.setOptions(getStackStyles(this.props.route.params.title))
+    }
+
+    async componentDidMount() {
+        this.getPermissionsAsync();
+    }
+
+    getPermissionsAsync = async () => {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA);
+        this.setState({ hasCameraPermission: status === 'granted' });
     }
 
     backNavigation = () => {
         this.props.navigation.goBack()
     }
 
+    // @ts-ignore
+    // REASON: Barcode scanner types unknown
+    handleBarCodeScanned = ({ type, data }) => {
+        this.setState({ scanned: true });
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    };
+
     render(): React.ReactNode {
+        const { hasCameraPermission, scanned } = this.state;
+
+        if (hasCameraPermission === null) {
+            return <RText>Requesting for camera permission</RText>;
+        }
+        if (hasCameraPermission === false) {
+            return <RText>No access to camera</RText>;
+        }
+
         return (
             <View style={Styles.screen}>
-                <RText>{this.props.route.params.title}</RText>
-                <Button title="Filter" onPress={this.backNavigation} />
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? () => { console.log("Already Scanned!")} : this.handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+                {scanned && (
+                    <Button
+                        title={'Tap to Scan Again'}
+                        onPress={() => this.setState({ scanned: false })}
+                    />
+                )}
             </View>
         )
     }
